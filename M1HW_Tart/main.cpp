57a6b3a7a8b644c7bbb073ebc00765f920841d52
tartl7709 <46724840+tartl7709@users.noreply.github.com>
2019-01-17 14:02:27 -0500

    M1HW1 Original

---
+++ M1HW_Tart/main.cpp
@@ -0,0 +1,22 @@
+// CSC 134
+// M1HW1
+// L Tart
+// 1/17/19
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    // provide a brief introduction
+
+    cout << "Hello everyone, my name is Lauren Tart" << endl;
+    cout << "I graduated from high school in " << 2017 << endl;
+    cout << "I am taking " << 4 << " classes at FTCC." << endl;
+    cout << "I have " << 1 << " sibling:" << endl;
+    cout << "\ta brother" << endl;
+    cout << endl;
+    return 0;
+}
