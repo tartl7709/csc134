8ea5a7a4152203095d7e9bcd276f8da3956ae7f9
tartl7709 <46724840+tartl7709@users.noreply.github.com>
2019-05-11 14:24:07 -0400

    M7T1
    Ch 1, Listing 1-1
    Ch 1, Listing 1-2
    Ch 2, Listing 2-1
    Ch 2, Listing 2-3

---
+++ M7T1_Tart/M7T1_Tart_Listing1-2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    string word;
+    ifstream infile("../MyFile.txt");
+    infile >> word;
+    cout << word << endl;
+    infile.close();
+
+    return 0;
+}
